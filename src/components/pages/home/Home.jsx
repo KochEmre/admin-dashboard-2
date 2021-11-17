8a57735e48c9d7ab994856986a0ed93b368c9867
KochEmre <51199984+KochEmre@users.noreply.github.com>
2021-11-18 02:43:22 +0300

    Sidebar düzenlemeleri yapıldı. Kartlar ve chart oluşturuldu, tamamlandı.

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,13 @@
+import Chart from "../../chart/Chart"
 import FeaturedInfo from "../../featuredInfo/FeaturedInfo"
 import "./home.css"
+import { userData } from "../../../dummyData.js";
 
 function Home() {
     return (
         <div className="home">
             <FeaturedInfo/>
+            <Chart data={userData} dataKey="Active User" title="User Analytics" grid />
         </div>
     )
 }
